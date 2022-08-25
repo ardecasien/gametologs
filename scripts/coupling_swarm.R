ad91bcba86c9ed6b23a106c94edb1f5c99a6cbad
Alex DeCasien <44094797+ardecasien@users.noreply.github.com>
2022-08-25 11:31:06 -0400

    Create coupling_swarm.R

---
+++ scripts/coupling_swarm.R
@@ -0,0 +1,15 @@
+library(reshape2)
+
+attrib = read.delim('GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt')
+keep_samples = subset(attrib, SMAFRZE == "RNASEQ")
+tissue.list = levels(as.factor(attrib$SMTSD))
+tissue.list = gsub(" ","",tissue.list)
+tissue.list = gsub("[(]","",tissue.list)
+tissue.list = gsub("[)]","",tissue.list)
+tissue.list = gsub("-","",tissue.list)
+
+for (i in 1:length(tissue.list)){
+	line = paste("Rscript /data/DNU/alex/gtex/XminusY.R", tissue.list[i], sep = " ")
+	write(line, file = "XminusY.swarm", append = TRUE, sep = "\n")
+	}
+  
